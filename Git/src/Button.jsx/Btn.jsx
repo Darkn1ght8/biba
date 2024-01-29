cbaa0ff00eb6906b828743254ace4605052c5c49
b1baa <b1baa.0880@gmail.com>
2024-01-29 20:48:55 +0500

    my third change

---
+++ Git/src/Button.jsx/Btn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Btn() {
+  return (
+    <div>Btn</div>
+    <div>
+        <input type="password" name="password" id="" />
+        <button type="submit">Submit</button>
+    </div>
+  )
+}
+
+export default Btn
